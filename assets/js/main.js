615f0a39c11d107c1eb527f025e7c5e88cc979b0
Chingiz Mammadli <memmedlicngz@gmail.com>
2016-11-22 22:05:36 +0400

    Merged Nigar's #2 pull request

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
-$(documet).ready(function(){
+$(document).ready(function(){
 
+// RESIZING all post-items
   $(window).resize(function(){
     if(window.width > 993)
       resizeElements();
@@ -12,7 +13,7 @@ $(documet).ready(function(){
   $(function resizeElements(){
     $("#category > .row").each(function(){
       var maxHeight=0;
-      console.log("isledi");
+      // console.log("isledi");
       $(this).find(".post-item").each(function(){
         if($(this).height() > maxHeight)
         maxHeight = $(this).height();
@@ -24,4 +25,25 @@ $(documet).ready(function(){
 
     });
   })
+// End of resizing all post-items
+
+// MENU
+var check=true;
+  $(".button-search").click(function(){
+    if(check){
+      $(".navbar .input-group").stop().animate({
+      width:"220px"},650);
+      $(".navbar .input-group input").stop().animate({
+      width:"200px"},650);
+      check=false;
+    }
+    else{
+      $(".navbar .input-group").stop().animate({
+      width:"20px"},650);
+      $(".navbar .input-group input").stop().animate({
+      width:0},650);
+      check=true;
+    }
+  });
+// end of menu
 });
