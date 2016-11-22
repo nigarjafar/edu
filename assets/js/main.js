b45e50759429fcb61b65c048f07482e2d61a7ccc
Nigar Jafar <nigar.c@code.edu.az>
2016-11-22 18:51:32 +0400

    search button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
+$(document).ready(function(){
+    var check=true;
+    $(".button-search").click(function(){
+        if(check){
+          $(".navbar .input-group").stop().animate({
+          width:"220px"},650);
+          $(".navbar .input-group input").stop().animate({
+          width:"200px"},650);
+          check=false;
+        }
+        else{
+          $(".navbar .input-group").stop().animate({
+          width:"20px"},650);
+          $(".navbar .input-group input").stop().animate({
+          width:0},650);
+          check=true;
+        }
 
+
+    });
+});
