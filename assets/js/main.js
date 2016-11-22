c0736a27a6725a28c53062423fc8a730bf915377
Chingiz Mammadli <memmedlicngz@gmail.com>
2016-11-22 21:53:28 +0400

    Category page partially ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
+$(documet).ready(function(){
 
+  $(window).resize(function(){
+    if(window.width > 993)
+      resizeElements();
+    else
+      $(".post-item").each(function(){
+        $(this).height(auto);
+      })
+  })
+
+  $(function resizeElements(){
+    $("#category > .row").each(function(){
+      var maxHeight=0;
+      console.log("isledi");
+      $(this).find(".post-item").each(function(){
+        if($(this).height() > maxHeight)
+        maxHeight = $(this).height();
+      });
+
+      $(this).find(".post-item").each(function(){
+        $(this).height(maxHeight);
+      });
+
+    });
+  })
+});
