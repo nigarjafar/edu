7c3cffbd032b1e452813cce78eee9f0043e0ed33
Chingiz Mammadli <memmedlicngz@gmail.com>
2016-11-27 21:48:16 +0400

    Post page,fixed 'aligning items' bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  alignItems();
 //Search button
 
 // ACTIONS slider
@@ -43,48 +42,8 @@ $(".add-reminder").click(function(e){
   //resizeElements();
 
 // RESIZING all post-items
-$(window).resize(function(){
-  location.reload();
-});
 
-function alignItems(){
-  var h = $(document).width()+17;
-  var itemsInRow;
-  var itemHeight;
-  var maxHeight=0;
-  var beginningOfRow=0;
-  var items = $(".post-item").length;
 
-  if(h >= 992){
-    itemsInRow = 3;
-  }
-  else if(h >= 768){
-    itemsInRow = 2;
-  }
-
-  for(var itemID=0;itemID<items;itemID++){
-    console.log(itemID+"--------------");
-    if(itemID % itemsInRow == 0){
-      beginningOfRow = itemID;
-      maxHeight=0;
-      console.log("NEW ROW");
-    }
-
-    for(var i=beginningOfRow;i<beginningOfRow+itemsInRow;i++){
-      itemHeight = $(".post-item:eq("+i+")").height();
-      if(itemHeight > maxHeight)
-        maxHeight = itemHeight;
-      console.log("item: "+itemHeight+"max: "+maxHeight);
-    }
-
-    for(var j=beginningOfRow;j<beginningOfRow+itemsInRow;j++){
-      $(".post-item:eq("+j+")").height(maxHeight);
-      console.log(itemID+" -> "+j);
-    }
-  }
-}
-
-// console.log("N: "+(n)+" cH: "+cH+" maxH: "+maxH+" cDist: "+cDist+" dist: "+dist);
 // End of resizing all post-items
 
 // MENU
