90dfd543b595cb328acb1f3269f4e58805927aad
Nigar Jafar <nigar.c@code.edu.az>
2016-11-29 22:03:23 +0400

    Home page ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,24 +50,24 @@ $(".add-reminder").click(function(e){
 
   $("body").click(function(event){
       if(event.target.id!="input-search"){
-      $(".navbar .input-group").stop().animate({
+      $(".navbar #full-nav-search").stop().animate({
       width:"80px"},650);
-      $("#input-search").stop().animate({
+      $("#full-nav-search #input-search").stop().animate({
       width:"67px"},650);
       }
 
   })
-  $("#input-search").focus(function(){
-      $(".navbar .input-group").stop().animate({
+  $("#full-nav-search #input-search").focus(function(){
+      $(".navbar #full-nav-search").stop().animate({
       width:"220px"},650);
-      $("#input-search").stop().animate({
+      $("#full-nav-search #input-search").stop().animate({
       width:"200px"},650);
     });
 
   $(".button-search").click(function(){
-      $(".navbar .input-group").stop().animate({
+      $(".navbar #full-nav-search").stop().animate({
       width:"80px"},650);
-      $("#input-search").stop().animate({
+      $("#full-nav-search #input-search").stop().animate({
       width:"67px"},650);
 
   })
@@ -93,21 +93,25 @@ $("#navbar-collapse .navbar-nav li a").click(function(){
 
 
 //Collapse menu open/close
-$("#collapse-button").click(function(){
-  if($("#navbar-collapse").css("left")=="-200px"){
-    $("#navbar-collapse").animate({ left:"0px"},500);
-  }
-  else{
-  $("#navbar-collapse").animate({ left:"-200px"},500);
-  }
-})
+// $("#collapse-button").click(function(){
+//   if($("#navbar-collapse").css("left")=="-200px"){
+//     $("#navbar-collapse").animate({ left:"0px"},500);
+//   }
+//   else{
+//   $("#navbar-collapse").animate({ left:"-200px"},500);
+//   }
+// })
 
-//width 975
+
+
+
+ToggleCollapseNavbar();
 NavbarCollapse();
 
 });
 
 $(window).resize( NavbarCollapse );
+$(window).resize( CloseCollapseNavbarWithoutEffect );
 
 function NavbarCollapse(){
   if( $("#top-collapse").css("display")!="none" ){
@@ -115,5 +119,35 @@ function NavbarCollapse(){
   }
   else{
     $("#navbar-collapse").prop("id","navbar")
+    $("#navbar").css("left","0px")
   }
+}
+
+function ToggleCollapseNavbar(){
+  $("#collapse-button").click(function(){
+  if($("body").css("left")=="0px"){
+  	OpenCollapseNavbar();
+  }
+  else{
+    CloseCollapseNavbar();
+  }
+})
+}
+
+function OpenCollapseNavbar(){
+
+  $("body").animate({ left:"200px"},500);
+  $("#top-collapse").animate({ left:"200px"},500);
+  $("#navbar-collapse").animate({ left:"0px"},500);
+}
+function CloseCollapseNavbar(){
+  $("body").animate({ left:"0"},500);
+  $("#top-collapse").animate({ left:"0"},500);
+  $("#navbar-collapse").animate({ left:"-200px"},500);
+}
+
+function CloseCollapseNavbarWithoutEffect(){
+  $("body").css("left","0");
+  $("#top-collapse").css("left","0")
+  $("#navbar-collapse").css("left","-200px")
 }
