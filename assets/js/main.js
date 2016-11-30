7bda5926a84c267299e6c9374eca1feb4acc91e1
Chingiz Mammadli <memmedlicngz@gmail.com>
2016-11-30 20:05:48 +0400

    Company details,company posts,image zoom

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,7 @@
 $(document).ready(function(){
 //Search button
 
-// ACTIONS slider
-$(".post-item .post-item-image").mouseenter(function(){
-  var height = $(this).find("img").height();
-  $(this).find("img, .post-item-actions").stop(true,false).animate({
-    top: "-"+height+"px",
-  },300);
-})
 
-$(".post-item .post-item-image").mouseleave(function(){
-  $(this).find("img, .post-item-actions").stop(true,false).animate({
-    top: "0px",
-  },200);
-})
-//End of ACTIONS slider
-
-// ACTIONS pop up
-var open = false;
-
-$("body").click(function(e){
-  if(open){
-    $("#popUp").css("display","none");
-    console.log("closed");
-    open=false;
-  }
-})
-
-$(".add-reminder").click(function(e){
-  if(open==false){
-    $("#popUp").css("display","initial");
-    console.log("opened");
-    open=true;
-    e.stopPropagation();
-  }
-
-})
-
-// End of ACTIONS pop up
-
-  //resizeElements();
-
-// RESIZING all post-items
-
-
-// End of resizing all post-items
 
 // Search
 
@@ -55,7 +12,6 @@ $(".add-reminder").click(function(e){
       $("#full-nav-search #input-search").stop().animate({
       width:"67px"},650);
       }
-
   })
   $("#full-nav-search #input-search").focus(function(){
       $(".navbar #full-nav-search").stop().animate({
@@ -135,6 +91,8 @@ function CloseCollapseNavbar(){
 }
 
 function CloseCollapseNavbarWithoutEffect(){
+  
+  $("#navbar-collapse .navbar-nav li .dropdown-menu").slideUp()
   $("body").css("left","0");
   $("#navbar-collapse .dropdown-menu").slideUp();
   $("#top-collapse").css("left","0")
